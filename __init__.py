604c4758ceb4455a17eac4a1ffb8094697caf8a5
Eugenio Lopez <eugenionlopez@gmail.com>
2019-09-30 16:49:52 -0300

    Change URLS to fit previous format

--- __init__.py
+++ __init__.py
@@ -13,9 +13,9 @@ def create_app(config_class=Config):
     from patpass_by_webpay import bp as patpass_by_webpay_bp
     from transaccion_completa_mall import bp as transaccion_completa_mall_bp
 
-    app.register_blueprint(transaccion_completa_bp, url_prefix='/transaccion_completa')
+    app.register_blueprint(transaccion_completa_bp, url_prefix='/fulltransaction')
     app.register_blueprint(patpass_by_webpay_bp, url_prefix='/patpass-webpay')
-    app.register_blueprint(transaccion_completa_mall_bp, url_prefix='/transaccion_completa/mall')
+    app.register_blueprint(transaccion_completa_mall_bp, url_prefix='/mallfulltransaction/')
 
     @app.route('/')
     def hello_world():
