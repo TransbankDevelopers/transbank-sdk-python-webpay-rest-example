66fa04c7976f48359c97e3ac3a4697aeeb7eba0f
Cesar Mayo <cesar.mayo18@gmail.com>
2019-11-04 16:09:44 -0300

    Add the refound method for webpay plus malll

--- webpay_plus_mall/routes.py
+++ webpay_plus_mall/routes.py
@@ -12,7 +12,6 @@ from transbank.webpay.webpay_plus.request import MallTransactionCreateDetails
 
 @bp.route('create', methods=['GET'])
 def show_create():
-    print('123123123123131231231231312')
     return render_template('/webpay/plus_mall/create.html', dt=dt, timedelta=timedelta,
                            child_commerce_codes=child_commerce_codes)
 
@@ -53,3 +52,25 @@ def webpay_plus_commit():
     print("response: {}".format(response))
 
     return render_template('/webpay/plus_mall/commit.html', token=token, response=response)
+
+@bp.route('refund-form', methods=['GET'])
+def refund_form():
+    return render_template('/webpay/plus_mall/refund-form.html', dt=dt, timedelta=timedelta,
+                           child_commerce_codes=child_commerce_codes)
+
+@bp.route("refund", methods=["POST"])
+def webpay_plus__mall_refund():
+    token = request.form.get("token_ws")
+    amount = request.form.get("amount")
+    buy_order = request.form.get("buy_order")
+    commerce_code = request.form.get("commerce_code")
+    print("refund for token_ws: {} by amount: {} buy_order: {} commerce_code: {}".format(token, amount,buy_order, commerce_code))
+
+    try:
+        response = MallTransaction.refund(token=token, amount=amount, child_commerce_code=commerce_code,
+                              child_buy_order=buy_order)
+        print("response: {}".format(response))
+
+        return render_template("webpay/plus_mall/refund.html", token=token, amount=amount, response=response)
+    except TransbankError as e:
+        print(e.message)
